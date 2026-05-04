b4c522c7114890704b44d7522158372b20e453ac
Lucas Lach <lachlucasklug@gmail.com>
2026-05-04 19:20:42 -0300

    up

--- Floresta.java
+++ Floresta.java
@@ -1,7 +1,18 @@
 package RPG;
 
+import java.util.Scanner;
+
 public class Floresta {
     public static void mostrar() {
+        Scanner Keys = new Scanner(System.in);
+        RPG nomePer = new RPG();
+        int vida = 100;
+        int dano;
+        String Mochila = "Espada, Escudo";
+        int IntegridadeDaEspada = 100;
+        int IntegridadeDoEscudo = 100;
+
+        nomePer.main(String[] args);
         System.out.println("Floresta encantada :)");
     }
 }
