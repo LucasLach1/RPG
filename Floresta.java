5f465dc800f35de963f6136d06c8e9250292c50c
Lucas Lach <lachlucasklug@gmail.com>
2026-05-05 12:50:55 -0300

    Update Floresta.java
    05/05/2026 12:50

--- Floresta.java
+++ Floresta.java
@@ -13,13 +13,38 @@ public class Floresta {
 
         
         String NomePersonagem = getNomePersonagem();
-        System.out.println("Floresta encantada :)");
-        System.out.println();
+        System.out.println("Floresta fechada");
+        //System.out.println(NomePersonagem);
+
+        System.out.println("Você vê na sua frente um castelo nasista");
+        System.out.println("O que vc faz? ");
+        System.out.printf("1.Fazer um teste de percepção \n");
+        System.out.printf("2.Entrar\n");
+        System.out.print("Opção: ");
+        int opcao = Keys.nextInt();
+
+        if(opcao==1){
+            int d20 = rolarD20();
+            if(d20 >= 17){
+                System.out.println("Você conseguiu");
+                System.out.println(d20);
+            }else{
+                System.out.println("Você não conseguiu");
+                System.out.println(d20);
+            }
+
+        }
+
+        
     }
     public static String getNomePersonagem() {
             Scanner Keys = new Scanner(System.in);
             System.out.print("Qual nome vc gostaria de dar ao seu personagem: ");
             String nomePersonagem = Keys.next();
             return nomePersonagem;
+    }
+
+    public static int rolarD20() {
+        return (int)(Math.random() * 20) + 1;
     }
 }
